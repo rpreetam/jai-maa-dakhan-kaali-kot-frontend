4fd5b616634065d5864fa0b03e379e4a55e74ce7
rpreetam <[rpreetam30@gmail.com]>
2024-12-08 09:45:34 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
+
+import "./App.css";
 
 function App() {
+  const [fullName, setFullName] = useState("");
+  const [amount, setAmount] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      await axios.post("http://localhost:5000/api/donations/adddonation", {
+        fullName,
+        amount,
+      }, {
+        'Content-Type': 'application/json'
+      });
+      alert("Donor added successfully!");
+      setFullName("");
+      setAmount("");
+    } catch (error) {
+      console.error("Error adding donor:", error);
+      alert("Failed to add donor.");
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="form-container">
+      <div>jai maa dakhan kaali</div>
+      <h2>Add Donor</h2>
+      <form onSubmit={handleSubmit}>
+        <label>Name:</label>
+        <input
+          type="text"
+          value={fullName}
+          onChange={(e) => setFullName(e.target.value)}
+          required
+        />
+
+        <label>Amount:</label>
+        <input
+          type="number"
+          value={amount}
+          onChange={(e) => setAmount(e.target.value)}
+          required
+        />
+        <button type="submit">Add Donor</button>
+      </form>
     </div>
   );
 }
